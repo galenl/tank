f61326be3759b4f0035a8d40f7f4e66f55b10963
galen <1470010843@qq.com>
2020-07-29 20:56:13 +0800

    通过键盘改变方向

---
+++ src/com/galen/tank/Dir.java
@@ -0,0 +1,6 @@
+package com.galen.tank;
+
+public enum Dir {
+
+    LEFT, UP, RIGHT, DOWN
+}
--- src/com/galen/tank/TankFrame.java
+++ src/com/galen/tank/TankFrame.java
@@ -11,8 +11,12 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
 
-    int x = 200;
-    int y = 200;
+    private int x = 200;
+    private int y = 200;
+
+    Dir dir = Dir.DOWN;
+
+    private static final int SPEED = 10;
 
     public TankFrame() {
         this.setSize(800, 600);
@@ -31,6 +35,22 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -57,6 +77,7 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         @Override
@@ -77,6 +98,14 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if (bL) dir = Dir.LEFT;
+            if (bU) dir = Dir.UP;
+            if (bR) dir = Dir.RIGHT;
+            if (bD) dir = Dir.DOWN;
         }
     }
 }
