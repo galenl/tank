b41a965b4ba8fd92912d7f45b850d5f9a86b0b18
galen <1470010843@qq.com>
2020-07-29 21:08:01 +0800

    抽象出坦克类

---
+++ src/com/galen/tank/Tank.java
@@ -0,0 +1,44 @@
+package com.galen.tank;
+
+import java.awt.*;
+
+public class Tank {
+    private int x;
+    private int y;
+    private Dir dir = Dir.DOWN;
+    private static final int SPEED = 10;
+
+    public Tank(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setDir(Dir dir) {
+        this.dir = dir;
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+}
