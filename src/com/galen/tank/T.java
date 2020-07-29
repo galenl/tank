b5c8070a8dc3fb75c4bc5932affb738140633a19
galen <1470010843@qq.com>
2020-07-29 20:04:23 +0800

    刷新窗口

--- src/com/galen/tank/T.java
+++
@@ -1,7 +0,0 @@
-package com.galen.tank;
-
-public class T {
-    public static void main(String[] args) {
-        TankFrame tf = new TankFrame();
-    }
-}
--- src/com/galen/tank/TankFrame.java
+++ src/com/galen/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.galen.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,11 +11,15 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
 
+    int x = 200;
+    int y = 200;
+
     public TankFrame() {
         this.setSize(800, 600);
         this.setResizable(false);
         this.setTitle("tank war");
         this.setVisible(true);
+        this.addKeyListener(new MyKeyListener());
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -24,6 +30,18 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200, 200, 50, 50);
+        g.fillRect(x, y, 50, 50);
+        x+=10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released");
+        }
     }
 }
